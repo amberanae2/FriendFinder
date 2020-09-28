b9b0a4789c4cc8867fdf5daa5f8ee3c60f4154fc
amber phillips <amber.ranae2@gmail.com>
2020-09-28 14:18:13 -0500

    updated new app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,150 +1,150 @@
 var friends = [
-    {
-      name: "Dora",
-      photo: "(https://www.impaktovisual.com.br/3260-large_default/display-dora-aventureira.jpg)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "5",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Arnold",
-      photo: "(https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/HeyArnold%21.jpg/150px-HeyArnold%21.jpg)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "5",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Courage",
-      photo: "(http://superawesomevectors.com/wp-content/uploads/2017/05/courage-the-cowardly-dog-vector-800x566.jpg)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "5",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Doug",
-      photo: "(https://timeentertainment.files.wordpress.com/2011/07/09_top10cartoonthemesongs1.jpg?w=360&h=240&crop=1)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "5",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Patty",
-      photo: "(https://vignette.wikia.nocookie.net/doug/images/4/41/Patti_2.jpg/revision/latest?cb=20140412213018)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "5",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Helga",
-      photo: "(http://assets2.ignimgs.com/2016/02/05/helgalistjpg-0d7b4e.jpg)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "5",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Jane",
-      photo: "(https://vignette.wikia.nocookie.net/x-menfanon/images/e/ea/Jean_Grey_%28X-Men_Evolution%29_2.jpg/revision/latest?cb=20100811013541)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "6",
-        "6",
-        "1",
-        "6",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Laura",
-      photo: "(https://vignette.wikia.nocookie.net/lara-croft-tomb-raider/images/6/66/Video_games_tomb_raider_lara_croft_desktop_7376x7040_hd-wallpaper-582915.jpg/revision/latest/scale-to-width-down/2000?cb=20121229002307)",
-      scores: [
-        "3",
-        "1",
-        "3",
-        "5",
-        "5",
-        "3",
-        "2",
-        "3",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Angelica",
-      photo: "(https://vignette.wikia.nocookie.net/rugratseries/images/2/2a/Angeilca.jpg/revision/latest?cb=20110202050259)",
-      scores: [
-        "5",
-        "1",
-        "3",
-        "5",
-        "7",
-        "7",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    }
-  
-  ];
-  
-  
-  module.exports = friends;
+  {
+    name: "Dora",
+    photo: "https://4.bp.blogspot.com/-1AIVRcNxgW8/WlveijN_EGI/AAAAAAABOzE/IPGTKESyzUMjj_q3LDUWwNU0Or-GelVAgCKgBGAs/s1600/Dora%2B1.png",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Arnold",
+    photo: "https://cdn2.vox-cdn.com/thumbor/nsq3LeWsLXAdrA8-53SxMx0M8Lg=/0x453:1061x1050/1280x720/cdn0.vox-cdn.com/uploads/chorus_image/image/47723111/81jtNhsOgfL._SL1500_.0.0.jpg",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Courage",
+    photo: "https://2.bp.blogspot.com/-GvuLbTv3zsk/T3wz_YHo8wI/AAAAAAAAEy0/mBjp0WTugOA/s1600/coragem+14.jpg",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Doug",
+    photo: "https://www.bing.com/images/search?view=detailV2&ccid=%2bEXtzMML&id=D28A762911008F37B56467633C5DE03225BBA6BA&thid=OIP.-EXtzMML0UuZWptBw6WSTgHaKH&mediaurl=https%3a%2f%2fvignette.wikia.nocookie.net%2fdisney%2fimages%2f3%2f3b%2fDoug001.gif%2frevision%2flatest%3fcb%3d20150716210125&exph=500&expw=366&q=doug&simid=608000926487873740&ck=F9D9B00BFBB97435EFD36401B5E315C3&selectedIndex=1&FORM=IRPRST&ajaxhist=0",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Patty",
+    photo: "(https://carboncostume.com/wordpress/wp-content/uploads/2017/11/Patti-Mayonnaise.jpg)",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Helga",
+    photo: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7377b4df-c8eb-45f1-86d6-79e63472164d/d4r6jyw-6b9fb983-6911-478b-b540-4f17f30af510.jpg/v1/fill/w_1600,h_2015,q_75,strp/helga_g__pataki_by_liviusquinky-d4r6jyw.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi83Mzc3YjRkZi1jOGViLTQ1ZjEtODZkNi03OWU2MzQ3MjE2NGQvZDRyNmp5dy02YjlmYjk4My02OTExLTQ3OGItYjU0MC00ZjE3ZjMwYWY1MTAuanBnIiwid2lkdGgiOiI8PTE2MDAiLCJoZWlnaHQiOiI8PTIwMTUifV1dfQ.A-F8x98r8bFHHU73TMkqZrYL8K5mwPNAKGydekrjSU4",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Gerald",
+    photo: "https://3.bp.blogspot.com/-tJBN14MndJ0/W43a1Drgh2I/AAAAAAABUDU/y_QzNTT6RUQYCd6TL9i-5bBvmWGVF9AawCKgBGAs/s1600/gerald_by_egondalatz.png",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Eugene",
+    photo: "https://th.bing.com/th/id/OIP.soGXmmh_Bt2dRrxDshczYQHaId?pid=Api&rs=1",
+    scores: [
+      "3",
+      "1",
+      "3",
+      "5",
+      "5",
+      "3",
+      "2",
+      "3",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Angelica",
+    photo: "https://i.pinimg.com/736x/05/24/93/0524936d31f98ca1de0ed84bdc311bc2--angelica-rugrats-rugrats-tattoo.jpg",
+    scores: [
+      "5",
+      "1",
+      "3",
+      "5",
+      "4",
+      "4",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  }
+
+];
+
+
+module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,47 +2,52 @@ var friends = require("../app/data/friends.js");
 
 
 
-module.exports = function(app) {
-  
+module.exports = function (app) {
 
-  app.get("/api/friends", function(req, res) {
+
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     var bestMatch = {
       name: "",
-      photo:"",
-      friendDifference
-    
+      photo: "",
+      friendDifference: Infinity
+
     };
     var userData = req.body;
     var userScores = userData.scores;
-    var userName= userData.name;
+    var userName = userData.name;
     var userPhoto = userData.photo;
+    console.log(userData);
 
-    var totalDifference = 0;
+    var totalDifference;
 
-    for(var i = 0; i <  friends.length; i++){
+    console.log(friends);
+
+    for (var i = 0; i < friends.length; i++) {
       var currentFriend = friends[i];
       totalDifference = 0;
       console.log(currentFriend.name);
 
-      totalDifference = 0;
-      console.log(currentFriend.name);
-      for (var j=0; j<currentFriend.scores.length; j++){
+
+      for (var j = 0; j < currentFriend.scores.length; j++) {
         var currentFriendScore = currentFriend.scores[j];
         var currentUserScore = userScores[j];
-        totalDifference+= Math.abs(parseInt(currentUserScore)- parseInt(currentFriendScore));
+        // console.log(userScores);
+
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
       }
-      if (totalDifference <= bestMatch.friendDifference){
+      if (totalDifference <= bestMatch.friendDifference) {
         bestMatch.name = currentFriend.name;
         bestMatch.photo = currentFriend.photo;
         bestMatch.friendDifference = totalDifference;
       }
     }
-    
-    friends.push(userData);
+    console.log(bestMatch);
+
+    // friends.push(userData);
     res.json(bestMatch);
 
   });
